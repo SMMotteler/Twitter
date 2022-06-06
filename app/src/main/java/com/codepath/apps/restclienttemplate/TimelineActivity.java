111efc4c0849fd480e73b349b3c50fead0c2c55b
Sarah Motteler <sarahmotteler@fb.com>
2022-06-06 16:43:25 -0700

    added logout button

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -4,8 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.apps.restclienttemplate.models.TweetsAdapter;
@@ -69,4 +71,19 @@ public class TimelineActivity extends AppCompatActivity {
             }
         });
     }
+
+    public void onLogoutButton(View v) {
+        Log.i("logout", "starting logout");
+        // forget who's logged in
+        TwitterApp.getTwitterClient(this).clearAccessToken();
+
+        // navigate backwards to Login screen
+        Intent i = new Intent(this, LoginActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // this makes sure the Back button won't work
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // same as above
+        startActivity(i);
+        finish();
+    }
+
+
 }
