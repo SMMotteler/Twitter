5ad559f1e39c25bcc4e43c14c5d25f5fbc41cc0c
Sarah Motteler <sarahmotteler@fb.com>
2022-06-07 16:55:44 -0700

    added tweeting functionality

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -2,34 +2,78 @@ package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.parceler.Parcels;
+
+import okhttp3.Headers;
+
 public class ComposeActivity extends AppCompatActivity {
+
+    public static final String TAG = "ComposeActivity";
 
     EditText tvTweetText;
     Button btnTweet;
+
+    TwitterClient client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compose);
+
+        client = TwitterApp.getTwitterClient(this);
         btnTweet = findViewById(R.id.btnTweet);
         tvTweetText = findViewById(R.id.tvTweetText);
-    }
+        btnTweet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String tweetText = tvTweetText.getText().toString();
+                if (tweetText.length() < 1) {
+                    Toast.makeText(ComposeActivity.this, "Tweet is empty", Toast.LENGTH_SHORT).show();
+                }
+                else if (tweetText.length() > 280) {
+                    Toast.makeText(ComposeActivity.this, "Tweet is over by "+(tweetText.length()-280)+" characters.", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(ComposeActivity.this, tweetText, Toast.LENGTH_SHORT).show();
+                    client.publishTweet(tweetText, new JsonHttpResponseHandler() {
+                        @Override
+                        public void onSuccess(int statusCode, Headers headers, JSON json) {
+                            Log.i(TAG, "onSuccess to publish tweet");
+                            try {
+                                Tweet tweet = Tweet.fromJson((json.jsonObject));
+                                Log.i(TAG, "Published tweet says: "+tweet.body);
+                                Intent i = new Intent();
+                                i.putExtra("tweet", Parcels.wrap(tweet));
+                                // set result code and bundle data for response
+                                setResult(RESULT_OK, i);
+                                // close the activity and pass data to the parent
+                                finish();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
 
-    public void onTweetButton(View view){
-        String tweetText = tvTweetText.getText().toString();
-        if (tweetText.length() < 1) {
-            Toast.makeText(this, "Tweet is empty", Toast.LENGTH_SHORT).show();
-        }
-        else if (tweetText.length() > 280) {
-            Toast.makeText(this, "Tweet is over by "+(tweetText.length()-280)+" characters.", Toast.LENGTH_SHORT).show();
-        }
-        else{
-            Toast.makeText(this, tweetText, Toast.LENGTH_SHORT).show();
-        }
+                        @Override
+                        public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                            Log.e(TAG, "onFailure to publish tweet", throwable);
+                        }
+                    });
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,6 +1,7 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -19,6 +20,7 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
 
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.parceler.Parcels;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,6 +30,7 @@ import okhttp3.Headers;
 public class TimelineActivity extends AppCompatActivity {
 
     public static final String TAG = "TimelineActivity";
+    private static final int REQUEST_CODE = 20;
 
     TwitterClient client;
     RecyclerView rvTweets;
@@ -63,9 +66,25 @@ public class TimelineActivity extends AppCompatActivity {
         if(item.getItemId() == R.id.compose_tweet){
             Toast.makeText(this, "Selected Item is Compose Tweet", Toast.LENGTH_SHORT).show();
             Intent i = new Intent(this, ComposeActivity.class);
-            startActivity(i);
+            startActivityForResult(i, REQUEST_CODE);
         }
         return true;
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == REQUEST_CODE && resultCode == RESULT_OK){
+            // Get data from the intent (tweet)
+            Tweet tweet = Parcels.unwrap(data.getParcelableExtra("tweet"));
+            // Update the RV with the tweet
+            // Modify data source of tweets
+            tweets.add(0, tweet);
+            // Update the adapter
+            adapter.notifyItemInserted(0);
+            rvTweets.smoothScrollToPosition(0);
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 
     private void populateHomeTimeline() {
